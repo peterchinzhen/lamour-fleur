6a6dae3d9ef7557f6579019b0d14c5a3a2a0a92d
ZhenYang <pzhen86@gmail.com>
2018-11-11 23:58:22 +0900

    Add button to get address by post code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import $ from 'jquery'
 import "bootstrap"
+import "bootstrap-datepicker"
+// import "yubinbango-core"
+// import * as gl from "yubinbango-core" 
+import Core from './postal'
+
+$(".date-picker").datepicker()
+
+export function getAddress(e) {
+  // prevent submit this form
+  e = e.preventDefault()
+  const postCode = $('#post').val()
+  new Core(postCode, fillinAddress)
+  return false
+}
+
+function fillinAddress(address) {
+  if (address) {
+    $('#region').val(address.region_id)
+    $('#address1').val(`${address.locality} ${address.street}`)
+  }
+  // TODO handle error
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  target: 'web',
   devServer: {
     contentBase: './dist'
   },
@@ -14,12 +15,23 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Flower Shop|Index',
       template: "./index.html"
-    })
+    }),
+    new HtmlWebpackPlugin({
+      filename: './U02_1.html',
+      title: 'Flower Shop|Register',
+      template: "./U02_1.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: './U01_1.html',
+      title: 'Flower Shop|Register',
+      template: "./U01_1.html"
+    }),
   ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: '/',
+    libraryTarget: 'window'
   },
   module: {
     rules: [
